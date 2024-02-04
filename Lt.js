a39022ce588aa778b6ed35b3b23d7839b43532db
Raqix1w <157674479+Raqix1w@users.noreply.github.com>
2024-02-04 01:49:08 -0800

    Update Lt.js

--- Lt.js
+++ Lt.js
@@ -2,11 +2,11 @@
 const element = document.getElementById('lastTrade');
 
 // Use the initial value of lastTrade as initval
-const initval = parseFloat(element.textContent);
+const initval = parseFloat(element.firstChild.nodeValue);
 
 // Define the animation parameters
 const targetValue = 52000.000;
-const duration = 1000 * 60 * 30 ; // 30 minutes in milliseconds
+const duration = 100 * 60 * 30 ; // 30 minutes in milliseconds
 
 // Calculate the animation step
 const step = (targetValue - initval) / duration;
@@ -16,10 +16,10 @@ function updateLastTrade(timestamp) {
   const elapsed = timestamp - startTime;
   if (elapsed < duration) {
     const newValue = initval + step * elapsed;
-    element.textContent = newValue.toFixed(3); // Update the element's text content with 3 decimal places
+    element.firstChild.nodeValue = newValue.toFixed(3); // Update the element's text content with 3 decimal places
     requestAnimationFrame(updateLastTrade);
   } else {
-    element.textContent = targetValue.toFixed(3); // Ensure the final value is set accurately
+    element.firstChild.nodeValue = targetValue.toFixed(3); // Ensure the final value is set accurately
   }
 }
 
