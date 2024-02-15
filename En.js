a124db7a0370d62d9b37a849d754c3ffe6d6ce2a
Raqix1w <157674479+Raqix1w@users.noreply.github.com>
2024-02-15 08:46:24 -0800

    Update En.js

--- En.js
+++ En.js
@@ -6,7 +6,7 @@ const initval = parseFloat(element.textContent.replace(/[^0-9.-]+/g,""));
 
 // Define the animation parameters
 const targetValue = 70000.00; // Target value with two decimal places
-const duration = 1000 * 30* 60; // 30 minutes in milliseconds
+const duration = 1000 * 30 * 60; // 30 minutes in milliseconds
 
 // Calculate the animation step
 const step = (targetValue - initval) / duration;
